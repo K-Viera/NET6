0b3bbc915d3d0abc559ae4d393bfc5050efb82c7
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-03-05 23:30:47 -0500

    other data types

--- VSCode/Chapter02-vscode/Numbers/Program.cs
+++ VSCode/Chapter02-vscode/Numbers/Program.cs
@@ -1,9 +1,13 @@
 ﻿// three variables that store the number 2 million
-int decimalNotation = 2_000_000;
-int binaryNotation = 0b_0001_1110_1000_0100_1000_0000; 
-int hexadecimalNotation = 0x_001E_8480;
+// int decimalNotation = 2_000_000;
+// int binaryNotation = 0b_0001_1110_1000_0100_1000_0000; 
+// int hexadecimalNotation = 0x_001E_8480;
 // check the three variables have the same value
 // both statements output true 
-Console.WriteLine($"{decimalNotation == binaryNotation}"); 
-Console.WriteLine(
-  $"{decimalNotation == hexadecimalNotation}");
+// Console.WriteLine($"{decimalNotation == binaryNotation}"); 
+// Console.WriteLine(
+//   $"{decimalNotation == hexadecimalNotation}");
+
+Console.WriteLine($"int uses {sizeof(int)} in the range {int.MinValue:N0} to {int.MaxValue:N0}");
+Console.WriteLine($"int uses {sizeof(double)} in the range {double.MinValue:N0} to {double.MaxValue:N0}");
+Console.WriteLine($"int uses {sizeof(decimal)} in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
---
+++ VSCode/Chapter02-vscode/Variables/Program.cs
@@ -0,0 +1,8 @@
+﻿object height = 1.88;// storing a double in an object
+object name = "Amir"; // storing a string in an object
+Console.WriteLine($"{name} is {height} metres tall.");
+//int length1 = name.Length; // gives compile error!
+int length2 = ((string)name).Length;
+Console.WriteLine($"{name} has {length2} characters.");
+dynamic something = "Ahmed";
+Console.WriteLine($"Length is {something.Length}");
