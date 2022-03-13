bd146131232adb1f524a4bd65371665ea9e64072
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-03-13 17:26:47 -0500

    variables and types

--- VSCode/Chapter02-vscode/Variables/Program.cs
+++ VSCode/Chapter02-vscode/Variables/Program.cs
@@ -1,8 +1,10 @@
-﻿object height = 1.88;// storing a double in an object
-object name = "Amir"; // storing a string in an object
-Console.WriteLine($"{name} is {height} metres tall.");
-//int length1 = name.Length; // gives compile error!
-int length2 = ((string)name).Length;
-Console.WriteLine($"{name} has {length2} characters.");
-dynamic something = "Ahmed";
-Console.WriteLine($"Length is {something.Length}");
+﻿using System.Xml;
+// good use of var because it avoids the repeated type
+// as shown in the more verbose second statement
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+// bad use of var because we cannot tell the type, so we
+// should use a specific type declaration as shown in
+// the second statement
+var file1 = File.CreateText("something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");
