7cac4a6dc6af512f9eeac1b119a9eb4e1e54b1e2
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-03-13 20:19:57 -0500

    argumemts in console application

---
+++ VSCode/Chapter02-vscode/Arguments/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+// arguments in dotnet command
+// dotnet run firstarg second-arg third:arg "fourth arg"
+Console.Write(typeof(string).Assembly.ImageRuntimeVersion);
+WriteLine($"There are {args.Length} arguments.");
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
+//dotnet run red yellow 50
+if (args.Length < 3)
+{
+    WriteLine("You must specify two colors and cursor size");
+    WriteLine("dotnet run red yellow 50");
+    return; // stop running
+}
+ForegroundColor = (ConsoleColor)Enum.Parse(
+ enumType: typeof(ConsoleColor),
+ value: args[0],
+ ignoreCase: true);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+ enumType: typeof(ConsoleColor),
+ value: args[1],
+ ignoreCase: true);
+
+try
+{
+ CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+ WriteLine("The current platform does not support change cursos size");
+}
+
