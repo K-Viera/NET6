2e54ca2fcfe8c3620f05e1541b2d19abe3fe26ca
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-06-15 19:44:23 -0500

    Tuples and constructors

--- VisualStudio/Chapter5/Chapter5/Person.cs
+++ VisualStudio/Chapter5/Chapter5/Person.cs
@@ -11,5 +11,32 @@ namespace Packt.Shared
         public string Name;
         public DateTime DateOfBirth;
         public List<Person> Children = new List<Person>();
+        public readonly string HomePlanet = "Earth";
+        public readonly DateTime Instantiated;
+
+        // constructors
+        public Person()
+        {
+            // set default values for fields
+            // including read-only fields
+            Name = "Unknown";
+            Instantiated = DateTime.Now;
+        }
+        public Person(string initialName, string homePlanet)
+        {
+            Name = initialName;
+            HomePlanet = homePlanet;
+            Instantiated = DateTime.Now;
+        }
+
+        //tuples
+        public (string, int) GetFruit()
+        {
+            return ("Apples", 5);
+        }
+        public (string Name, int Number) GetNamedFruit()
+        {
+            return (Name: "Apples", Number: 5);
+        }
     }
 }
--- VisualStudio/Chapter5/PeopleApp/Program.cs
+++ VisualStudio/Chapter5/PeopleApp/Program.cs
@@ -2,16 +2,40 @@
 using Packt.Shared;
 using static System.Console;
 
-BankAccount.InterestRate = 0.012M; // store a shared value
-BankAccount jonesAccount = new(); // C# 9.0 and later
-jonesAccount.AccountName = "Mrs. Jones";
-jonesAccount.Balance = 2400;
-WriteLine(format: "{0} earned {1:C} interest.",
-  arg0: jonesAccount.AccountName,
-  arg1: jonesAccount.Balance * BankAccount.InterestRate);
-BankAccount gerrierAccount = new();
-gerrierAccount.AccountName = "Ms. Gerrier";
-gerrierAccount.Balance = 98;
-WriteLine(format: "{0} earned {1:C} interest.",
-  arg0: gerrierAccount.AccountName,
-  arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+//BankAccount.InterestRate = 0.012M; // store a shared value
+//BankAccount jonesAccount = new(); // C# 9.0 and later
+//jonesAccount.AccountName = "Mrs. Jones";
+//jonesAccount.Balance = 2400;
+//WriteLine(format: "{0} earned {1:C} interest.",
+//  arg0: jonesAccount.AccountName,
+//  arg1: jonesAccount.Balance * BankAccount.InterestRate);
+//BankAccount gerrierAccount = new();
+//gerrierAccount.AccountName = "Ms. Gerrier";
+//gerrierAccount.Balance = 98;
+//WriteLine(format: "{0} earned {1:C} interest.",
+//  arg0: gerrierAccount.AccountName,
+//  arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+//Constructors
+//Person blankPerson = new();
+//WriteLine(format:
+//  "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+//  arg0: blankPerson.Name,
+//  arg1: blankPerson.HomePlanet,
+//  arg2: blankPerson.Instantiated);
+
+//Tuples
+Person bob = new();
+(string, int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+//inferring tuple names
+var thing1 = ("Neville", 4);
+WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+var thing2 = (bob.Name, bob.Children.Count);
+WriteLine($"{thing2.Name} has {thing2.Count} children.");
+//Deconstructing tuples
+(string fruitName, int fruitNumber) = bob.GetFruit();
+WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
