582d66dbe9b034ce9662b318572de4d900a4406d
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-06-16 19:12:53 -0500

    Chapter 6 operator

---
+++ VisualStudio/Chapter6/PeopleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+Person harry = new() { Name = "Harry" };
+Person mary = new() { Name = "Mary" };
+Person jill = new() { Name = "Jill" };
+
+// call static method
+Person baby2 = Person.Procreate(harry, jill);
+// call an operator
+Person baby3 = harry * mary;
