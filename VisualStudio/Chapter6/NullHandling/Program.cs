9ac9e5ccbc9a8bc361e46faa35a82b2121d28a9a
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-06-18 21:33:27 -0500

    nulls and reference type nulleable

---
+++ VisualStudio/Chapter6/NullHandling/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Address address = new();
+address.Building = null;
+address.Street = null;
+address.City = "London";
+address.Region = null;
+class Address
+{
+    public string? Building;
+    public string Street = string.Empty;
+    public string City = string.Empty;
+    public string Region = string.Empty;
+}
