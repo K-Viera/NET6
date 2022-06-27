8d8d56d1e0924223f48e6024e9eb5f43d6b7fc3c
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-06-27 15:52:06 -0500

    Disposing file resources with dispose and using statements

--- VisualStudio/Chapter09/WorkingWithStreams/Program.cs
+++ VisualStudio/Chapter09/WorkingWithStreams/Program.cs
@@ -7,52 +7,77 @@ WorkWithXml();
 
 static void WorkWithXml()
 {
-    //define a file to write to
-    string xmlFile = Path.Combine(Environment.CurrentDirectory, "stream.xml");
-    //create a file stream
-    FileStream xmlFileStream = File.Create(xmlFile);
-    //wrap the file stream in a XML writer helper
-    //and automatically indent nested elements
-    XmlWriter xmlWriter = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
-    //write the XML declaration
-    xmlWriter.WriteStartDocument();
-    //write a root element 
-    xmlWriter.WriteStartElement("callsigns");
-    // enumerate the strings writing each one to the stream
-    foreach (string item in Viper.Callsigns)
+    FileStream? xmlFileStream = null;
+    XmlWriter? xmlWriter = null;
+    try
     {
-        xmlWriter.WriteElementString("callsign", item);
+        //define a file to write to
+        string xmlFile = Path.Combine(Environment.CurrentDirectory, "stream.xml");
+        //create a file stream
+        xmlFileStream = File.Create(xmlFile);
+        //wrap the file stream in a XML writer helper
+        //and automatically indent nested elements
+        xmlWriter = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+        //write the XML declaration
+        xmlWriter.WriteStartDocument();
+        //write a root element 
+        xmlWriter.WriteStartElement("callsigns");
+        // enumerate the strings writing each one to the stream
+        foreach (string item in Viper.Callsigns)
+        {
+            xmlWriter.WriteElementString("callsign", item);
+        }
+        xmlWriter.WriteEndElement();
+        xmlWriter.Close();
+        xmlFileStream.Close();
+        // output all the contents of the file
+        Console.WriteLine("{0} contains {1:N0} bytes.",
+          arg0: xmlFile,
+          arg1: new FileInfo(xmlFile).Length);
+        Console.WriteLine(File.ReadAllText(xmlFile));
     }
-    xmlWriter.WriteEndElement();
-    xmlWriter.Close();
-    xmlFileStream.Close();
-    // output all the contents of the file
-    Console.WriteLine("{0} contains {1:N0} bytes.",
-      arg0: xmlFile,
-      arg1: new FileInfo(xmlFile).Length);
-    Console.WriteLine(File.ReadAllText(xmlFile));
+    catch (Exception ex)
+    {
+        // if the path doesn't exist the exception will be caught
+        Console.WriteLine($"{ex.GetType()} says {ex.Message}");
+    }
+    finally
+    {
+        if (xmlWriter != null)
+        {
+            xmlWriter.Dispose();
+            Console.WriteLine("The XML writer's unmanaged resources have been disposed.");
+            if (xmlFileStream != null)
+            {
+                xmlFileStream.Dispose();
+                Console.WriteLine("The file stream's unmanaged resources have been disposed.");
+            }
+        }
+    }
+
 }
-
-
 
 static void WorkWithText()
 {
     //define a file to write to
     string textFile = Path.Combine(Environment.CurrentDirectory, "stream.txt");
     //create a text file and return a helper writer
-    StreamWriter text = File.CreateText(textFile);
-    // enumerate the strings, writing each one
-    // to the stream on a separate line
-    foreach (string item in Viper.Callsigns)
+    using (StreamWriter text = File.CreateText(textFile))
     {
-        text.WriteLine(item);
+        // enumerate the strings, writing each one
+        // to the stream on a separate line
+        foreach (string item in Viper.Callsigns)
+        {
+            text.WriteLine(item);
+        }
+        text.Close(); // release resources
+                      // output the contents of the file
     }
-    text.Close(); // release resources
-                  // output the contents of the file
     Console.WriteLine("{0} contains {1:N0} bytes.",
-      arg0: textFile,
-      arg1: new FileInfo(textFile).Length);
+     arg0: textFile,
+     arg1: new FileInfo(textFile).Length);
     Console.WriteLine(File.ReadAllText(textFile));
+
 }
 static class Viper
 {
