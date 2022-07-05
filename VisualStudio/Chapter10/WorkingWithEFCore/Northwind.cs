6cb88fc0792ea6e70affd73f89506e30cd95f7a5
Kevin Viera <53414453+K-Viera@users.noreply.github.com>
2022-07-04 19:54:56 -0500

    Basics of Entity framework

---
+++ VisualStudio/Chapter10/WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; // DbContext, DbContextOptionsBuilder
+using static System.Console;
+
+namespace Packt.Shared
+{
+    internal class Northwind : DbContext
+    {
+        protected override void OnConfiguring(
+          DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "Data Source=COMEDLAP0003\\SQLEXPRESS;" +
+        "Initial Catalog=Northwind;" +
+        "Integrated Security=true;" +
+        "MultipleActiveResultSets=true;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+        //these properties map to tables in the database
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            //example of using Fluent API instead of attibutes
+            //to limit the length of a category name to 15
+            modelBuilder.Entity<Category>()
+                .Property(category=>category.CategoryName)
+                .IsRequired()
+                .HasMaxLength(15);
+            // global filter to remove discontinued products
+            modelBuilder.Entity<Product>()
+              .HasQueryFilter(p => !p.Discontinued);
+        }
+    }
+}
---
+++ VisualStudio/Chapter10/WorkingWithEFCore/Program.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore; // Include extension method
+using Packt.Shared;
+using static System.Console;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+QueryingCategories();
+//FilteredIncludes();
+//QueryingProducts();
+
+static void QueryingProducts()
+{
+    using (Northwind db = new())
+    {
+        WriteLine("Products that cost more than a price, highest at top.");
+        string? input;
+        decimal price;
+        do
+        {
+            Write("Enter a product price: ");
+            input = ReadLine();
+        } while (!decimal.TryParse(input, out price));
+        IQueryable<Product>? products = db.Products?
+          .Where(product => product.Cost > price)
+          .OrderByDescending(product => product.Cost);
+        if (products is null)
+        {
+            WriteLine("No products found.");
+            return;
+        }
+        WriteLine($"ToQueryString: {products.ToQueryString()}");
+        foreach (Product p in products)
+        {
+            WriteLine(
+              "{0}: {1} costs {2:$#,##0.00} and has {3} in stock.",
+              p.ProductId, p.ProductName, p.Cost, p.Stock);
+        }
+    }
+}
+static void FilteredIncludes()
+{
+    using (Northwind db = new())
+    {
+        ILoggerFactory loggerFactory = db.GetService<ILoggerFactory>();
+        loggerFactory.AddProvider(new ConsoleLoggerProvider());
+        Write("Enter a minimum for units in stock: ");
+        string unitsInStock = ReadLine() ?? "10";
+        int stock = int.Parse(unitsInStock);
+        IQueryable<Category>? categories = db.Categories?
+          .Include(c => c.Products.Where(p => p.Stock >= stock));
+        if (categories is null)
+        {
+            WriteLine("No categories found.");
+            return;
+        }
+        foreach (Category c in categories)
+        {
+            WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock.");
+            foreach (Product p in c.Products)
+            {
+                WriteLine($"  {p.ProductName} has {p.Stock} units in stock.");
+            }
+        }
+    }
+}
+
+static void QueryingCategories()
+{
+    try
+    {
+        using (Northwind db = new())
+        {
+            ILoggerFactory loggerFactory = db.GetService<ILoggerFactory>();
+            loggerFactory.AddProvider(new ConsoleLoggerProvider());
+            Console.WriteLine("Categories and how many products they have:");
+            // a query to get all categories and their related products
+            IQueryable<Category>? categories = db.Categories?
+              .Include(c => c.Products);
+            if (categories is null)
+            {
+                Console.WriteLine("No categories found.");
+                return;
+            }
+            // execute query and enumerate results
+            foreach (Category c in categories)
+            {
+                Console.WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+            }
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
