d5054528e0175c5ffb0feca6986b6f1bb1a68efd
K-Viera <kenvi.009@gmail.com>
2022-07-27 17:33:36 -0500

    Create Web API with MongoDB and Docker

---
+++ VisualStudio/ApiWithMongo/ApiWithMongo/Controllers/BooksController.cs
@@ -0,0 +1,83 @@
+﻿using ApiWithMongo.Models;
+using ApiWithMongo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWithMongo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly BooksService _booksService;
+
+        public BooksController(BooksService booksService) =>
+            _booksService = booksService;
+
+        [HttpGet]
+        public async Task<List<Book>> Get() 
+        {
+            var prueba= await _booksService.GetAsync();
+            return prueba;
+        }
+            
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<Book>> Get(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Book newBook)
+        {
+            try
+            {
+                await _booksService.CreateAsync(newBook);
+
+                return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+            }
+            catch (Exception e) {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, Book updatedBook)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            updatedBook.Id = book.Id;
+
+            await _booksService.UpdateAsync(id, updatedBook);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var book = await _booksService.GetAsync(id);
+
+            if (book is null)
+            {
+                return NotFound();
+            }
+
+            await _booksService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+}
